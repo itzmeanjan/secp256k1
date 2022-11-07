141bf7a623c5adbe18b5c2a69757cd868ec822f4
Anjan Roy <hello@itzmeanjan.in>
2022-11-07 11:13:23 +0400

    corrected ecdsa siging
    Signed-off-by: Anjan Roy <hello@itzmeanjan.in>

--- sign.py
+++ sign.py
@@ -6,7 +6,7 @@ from hashlib import sha3_256
 from field import Field
 from point import Point
 from secrets import randbelow
-from consts import PRIME, Gx, Gy
+from consts import Gx, Gy, n
 
 
 def sign(skey: int, msg: bytes) -> Tuple[int, int]:
@@ -14,28 +14,24 @@ def sign(skey: int, msg: bytes) -> Tuple[int, int]:
     Given ECDSA secret key ( a 256 -bit integer ) and a message `m`, this routine attempts to
     perform randomized signing, while hashing the message using SHA3-256.
 
-    Returns (r, s) two 256 -bit integers ( ∈ [0, PRIME) ), as ECDSA signature.
+    Returns (r, s) two 256 -bit integers ( ∈ [0, n) ), as ECDSA signature.
 
     Follows scheme described https://cryptobook.nakov.com/digital-signatures/ecdsa-sign-verify-messages#ecdsa-sign
     """
     h = sha3_256(msg).digest()
     h = int.from_bytes(h, byteorder="big")
-    h = h % PRIME
+    h = h % n
 
-    k = 1 + randbelow(PRIME - 1)
+    k = 1 + randbelow(n - 1)
 
     g = Point.fromAffine(Field.from_num(Gx), Field.from_num(Gy))
     r = g.mulScalar(k)
-    r = r.toAffine()[0]
+    r = r.toAffine()[0].to_num()
 
-    t0 = Field.from_num(skey)
-    t2 = Field.from_num(h)
-    t3 = Field.from_num(k)
+    k_inv = pow(k, -1, n)
+    s = ((h + r * skey) * k_inv) % n
 
-    t4 = t3.inv() * (t2 + r * t0)
-    s = t4.to_num()
-
-    return r.to_num(), s
+    return r, s
 
 
 if __name__ == "__main__":
